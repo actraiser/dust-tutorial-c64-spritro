4a1eb98a8c22bd96475d192e87f9032fb9a84996
Rocco <rocco@dileo.info>
2013-05-12 23:48:00 +0200

    config sprites

--- code/config_symbols.asm
+++ code/config_symbols.asm
@@ -4,6 +4,18 @@
 ; for some people it is easier to remember words then hex numbers
 ;================================================================
 
+;============================================================
+; symbols
+;============================================================
+
+; usable zero page bytes:
+; Free: $02, $FB-$FE
+; Temp Storage: $22-$29, $57-$60 (used by some Kernel Routines) 
+; If no datasette or RS232 is used: $9E-$9F, $A5-$A7, $A9-$AB, $B0-$B6, $F7-$FA
+; Used by attache devices: $90-$C2 (except $A0-$A2 used for internal clock)
+; Required for Basic: $03-$56 und $73-$8B.
+
+
 screen_ram = $0400			; location of screen ram
 address_sprites = $2000		;loading address for ship sprite
 address_sid = $1001 	  ; loading address for sid tune
--- code/sub_move_ship.asm
+++ code/sub_move_ship.asm
@@ -3,9 +3,11 @@
 ; high bit is initially set to start at outer right position 
 ;============================================================
 
-move_ship  ldx #$80 
-		   stx $d000
-           stx $d001
-           
+move_ship  inc $07f8
+		   cmp #$90
+		   bne reset_ship 
+		   rts
 
+reset ship lda #$80
+		   sta $07f8
            rts
