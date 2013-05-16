b3bfe123930b4cdd01bb9c47df1a0bfd15893ff8
actraiser <actraiser@dustlayer.com>
2013-05-16 17:04:29 +0200

    fixes

--- code/data_text.asm
+++ code/data_text.asm
@@ -2,7 +2,4 @@
 
 line1    !scr "     ]]] dustlayer.com presents ]]]          "
 line2    !scr " ]]] spritro tutorial by actraiser ]]]       " 
-
 line3    !scr "    press u and d to control sprite!         " 
-
-
